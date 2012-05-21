f2945104c499d68f95388481424eb874f74d767a
Vladimir Pouzanov <farcaller@gmail.com>
2012-05-21 15:10:57 +0300

    A first working version

--- lib/motion-redgreen.rb
+++ lib/motion-redgreen.rb
@@ -2,6 +2,9 @@ require "motion-redgreen/version"
 require "motion-redgreen/config"
 
 Motion::Project::App.setup do |app|
-  #app.files << File.expand_path(File.dirname(__FILE__) + '/motion-redgreen/ansiterm.rb')
+  app.development do
+    app.files << File.expand_path(File.dirname(__FILE__) + '/motion-redgreen/ansiterm.rb')
+    app.files << File.expand_path(File.dirname(__FILE__) + '/motion-redgreen/string.rb')
+  end
 end
 
